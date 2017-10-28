008938f70631231243bc263914677bdbd0e69cf5
Silvije Skudar <sil.skud@gmail.com>
2017-10-28 23:36:07 +0200

    Splitting functionalities

--- .shellscripts/base_script.sh
+++ .shellscripts/base_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Disable PC speaker bell - Xorg
-sudo echo "xset -b" > /etc/xprofile
-
 bash install_essentials.sh
 bash copy_dotfiles.sh
 bash install_optional.sh
---
+++ .shellscripts/disable_bell.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Disable PC speaker bell - Xorg
+sudo echo "xset -b" > /etc/xprofile
