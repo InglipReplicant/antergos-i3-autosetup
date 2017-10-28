7db8ee85b47631d8b99407945b21399f69982d1a
Silvije Skudar <sil.skud@gmail.com>
2017-10-28 23:29:56 +0200

    Fixing file name

--- .shellscripts/install_optional.sh
+++ .shellscripts/install_optional.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 bash ~/.shellscripts/optional_scripts/install-VSCode.sh
-bash ~/.shellscripts/optional_scripts/install-obs-studio.sh
+bash ~/.shellscripts/optional_scripts/install-obs.sh
 bash ~/.shellscripts/optional_scripts/install-pinta.sh
